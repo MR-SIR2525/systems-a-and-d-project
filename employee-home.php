09c098f4e70d2abe572232c22a3441cea5e2f195
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-11-13 23:22:16 -0600

    good emp home, custom flex css for grid pattern

--- employee-home.php
+++ employee-home.php
@@ -38,37 +38,42 @@
 
 
   <!-- background color and large text to the whole section -->
-  <section class="w3-sand w3-grayscale w3-large px-48">
+  <section class="w3-light-grey w3-grayscale w3-large px-48 w3-container">
     <header class="w3-container mt-55" id="home">
       <h1>Kaitaia Publishing Collective - Employee Home</h1>
+      <p class="w3-center w3-text-grey">Select a menu card to get started:</p>
     </header>
 
-    <!-- Menu Grid -->
-    <div class="w3-container w3-padding-32">
-      <div class="w3-row-padding w3-center">
+    <!-- Custom Flexbox Menu Grid -->
+    <div class="menu-grid">
+      <?php
+        $menu_items = array(
+          "Manuscripts Awaiting Evaluation",
+          "Cost and Schedule Estimation",
+          "Approve/Deny Estimates",
+          "Draft and Send Contracts",
+          "Publishing Status"
+        );
 
-        <?php
-          $menu_items = array(
-            "Manuscripts Awaiting Evaluation",
-            "Cost and Schedule Estimation",
-            "Approve/Deny Estimates",
-            "Draft and Send Contracts",
-            "Publishing Status"
-          );
+        $item_desc = array( //parallel array to menu_items
+          "Manuscripts for Maria to pass or reject",
+          "to pass on to Management for another evaluation",
+          "Management approves or rejects the estimates",
+          "Legal team uploads drafts/final version for send off",
+          "View or update the printing/publishing status"
+        );
 
-          for ($i = 0; $i < count($menu_items); $i++) {
-            print '
-              <div class="w3-third w3-margin-bottom">
-                <a href="#link' . $i . '" class="w3-card-4 br-10 w3-hover-grey w3-white w3-block">
-                  <h3 class="w3-text-dark-grey">' . $menu_items[$i] . '</h3>
-                </a>
-              </div>
-            ';
-          }
-
-        ?>
-        
-      </div>
+        foreach ($menu_items as $index => $item) {
+          print '
+            <div class="menu-card w3-card w3-round-large w3-white w3-hover-shadow w3-hover-light-grey">
+              <a href="#link' . $index . '" class="menu-card-link">
+                <h3 class="w3-text-dark-grey">' . $item . '</h3>
+                <p class="w3-text-grey w3-medium">' . $item_desc[$index] . '</p>
+              </a>
+            </div>
+          ';
+        }
+      ?>
     </div>
   </section>
   <!-- End page content -->
