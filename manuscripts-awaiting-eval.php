34a2f8730fb2f3cc5a88f11754cd98bda2d50ebc
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-11-14 00:36:54 -0600

    simulate row being selected

--- manuscripts-awaiting-eval.php
+++ manuscripts-awaiting-eval.php
@@ -59,14 +59,24 @@
             $dates = array("2023-11-10", "2023-11-11", "2023-11-12", "2023-11-13");
 
             for ($i = 0; $i < count($titles); $i++) {
-              echo '<tr>
-                <td><input type="checkbox" class="w3-check" /></td>
+              // simulate that we selected this row.
+              if ($titles[$i] == "The Great Adventure") {
+                print '<tr class="w3-grey w3-hover-grey">';
+              }
+              else {
+                print '<tr>';
+              }
+
+              print '
+                <td><input type="checkbox" class="w3-check" ' . ($titles[$i] == "The Great Adventure" ? 'checked' : '') . '/></td>
                 <td>' . $titles[$i] . '</td>
                 <td>' . $authors[$i] . '</td>
                 <td>' . $emails[$i] . '</td>
                 <td>' . $dates[$i] . '</td>
                 <td>
-                  <a href="#" class="w3-button w3-small w3-blue w3-round w3-margin-left">⬇</a>
+                  <a href="#" class="w3-button w3-small w3-blue w3-round w3-margin-left">
+                    <i class="fa fa-download"></i>
+                  </a>
                 </td>
               </tr>';
             }
