3a240463b614aa67dd21aaf4cc828a38c416bcb6
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-11-17 22:03:55 -0600

    author upload page has logged in check, formatting

--- author-upload-manuscript.php
+++ author-upload-manuscript.php
@@ -16,36 +16,36 @@
     include 'templates/nav.php'; 
   ?>
 
-  <section class="w3-content w3-margin-top w3-padding-top-64 flex-center">
+  <section class="w3-content mb-64 flex-center" style="margin-top: 75px;">
     <div class="w3-card-4 w3-white w3-round-large w3-padding form-card">
       <header class="w3-center w3-border-bottom w3-border-red w3-padding-16">
         <h2 class="w3-text-dark-grey">Author Personal Info and Manuscript</h2>
         <p>Fill out your information and upload your manuscript.</p>
       </header>
   
-      <form id="manuscript-upload" action="author-submitted.php" method="POST" class="w3-container w3-padding-large">
-        <!-- First Name -->
-        <div class="w3-margin-bottom">
-          <label for="first_name" class="w3-text-dark-grey"><b>First name</b></label>
-          <input type="text" id="first_name" name="first_name" required placeholder="Enter first name"
-            class="w3-input w3-border w3-round">
-        </div>
-  
-        <!-- Last Name -->
-        <div class="w3-margin-bottom">
-          <label for="last_name" class="w3-text-dark-grey"><b>Last name</b></label>
-          <input type="text" id="last_name" name="last_name" required placeholder="Enter last name"
-            class="w3-input w3-border w3-round">
-        </div>
-  
-        <!-- Email -->
-        <div class="w3-margin-bottom">
-          <label for="authorEmail" class="w3-text-dark-grey"><b>Email</b></label>
-          <input type="text" id="authorEmail" name="authorEmail" required placeholder="Enter email"
-            class="w3-input w3-border w3-round">
-          <p class="w3-small w3-text-grey mt-0">Be sure to use an email we can reach you at.</p>
-        </div>
+      <?php
+        // isAuthor should be initialized in nav.php, so we check it
+        if ($isAuthor) {
+          print '
+            <div class="w3-margin-bottom w3-center">
+              <p class="w3-text-dark-grey"><b>' . $_SESSION['author_first_name'] . ' ' . $_SESSION['author_last_name'] . '</b></p>
+              <p class="w3-text-dark-grey"><b>' . $_SESSION['author_email'] . '</b></p>
+            </div>';
+        }
+        else {
+          print '
+            <div class="w3-margin-bottom w3-center w3-red w3-round w3-padding">
+              <b>You need to be logged in to upload a manuscript</b>
+            </div>
+            
+            <div class="w3-center w3-margin-bottom">
+              <button onclick="document.getElementById(\'authorLoginModal\').style.display=\'block\'" 
+                class="w3-button w3-blue w3-card w3-underline" title="Sign in as Author">Author Login/Signup</button>
+            </div>';
+        }
+      ?>
 
+      <form id="manuscript-upload" action="author-submitted.php" method="POST" class="w3-container w3-padding-large">
         <div class="w3-margin-bottom">
           <!-- Auto width -->
           <label for="manuscriptFile" class="w3-text-dark-grey"><b>Manuscript Upload</b></label>
@@ -72,8 +72,14 @@
         </div>
   
         <!-- Submit Button -->
-        <button type="submit" class="w3-button w3-red w3-hover-dark-red w3-round-large w3-padding-large w3-block">Submit
-          Data</button>
+        <?php 
+          if ($isAuthor) {
+            print '
+            <button type="submit" class="w3-button w3-green w3-hover-dark-red w3-round-large w3-padding-large w3-block">
+              Submit Data
+            </button>';
+          }
+        ?>
       </form>
   
       <!-- Success/Error Message -->
@@ -84,6 +90,8 @@
 
   <!-- Footer -->
   <?php include 'templates/footer.php'; ?>
+  <?php include 'author-login.html'; ?>
+  <?php include 'author-signup.html'; ?>
 
 
 </body>
